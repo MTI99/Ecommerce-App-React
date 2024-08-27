a9357832baa78ad427b50b0839e02c7a2a9ca0f6
unknown <mt.eg99@gmail.com>
2024-08-28 01:39:57 +0300

    'handleCartCount'

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,10 +7,6 @@ import MainSlider from "../MainSlider/MainSlider";
 export default function Home() {
   return (
     <>
-      <h2 className="mx-auto text-3xl text-center my-10 font-bold border-b-2 pb-2 border-blue-500 w-fit">
-        Home
-      </h2>
-
 
       <div className="container">
         <MainSlider />
